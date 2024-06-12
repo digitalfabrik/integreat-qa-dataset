6223c611ea2e818b043f9875039c6e64c9306f1b
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-06-12 13:24:22 +0200

    Modify classifier

--- tools/classifier.py
+++ tools/classifier.py
@@ -4,8 +4,7 @@ import evaluate
 import pandas as pd
 from datasets import Dataset
 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
-
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding, pipeline
 BERT = 'google-bert/bert-base-uncased'
 DEBERTA = 'deberta-v3-large'
 model_name = DEBERTA
@@ -16,10 +15,17 @@ MODEL_PATH = f'/hpc/gpfs2/scratch/g/coling/models/{model_name}'
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, do_lower_case=True)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, num_labels=2)
+output_dir=f'/hpc/gpfs2/scratch/u/kleinlst/thesis/integreat-qa-dataset/train/{model_name}'
 training_arguments = TrainingArguments(
-    output_dir=f'/hpc/gpfs2/scratch/u/kleinlst/thesis/integreat-qa-dataset/train/{model_name}',
+    output_dir=output_dir,
     evaluation_strategy='steps',
-    per_device_train_batch_size=8,
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    save_strategy="epoch",
+    load_best_model_at_end=True,
     do_train=True,
 )
 metric = evaluate.load('/hpc/gpfs2/scratch/u/kleinlst/thesis/integreat-qa-dataset/evaluate/metrics/f1')
@@ -58,6 +64,7 @@ def compute_metrics(eval_pred):
 if __name__ == '__main__':
     language = 'en'
     train, dev, test = load_splits(language)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     trainer = Trainer(
         model=model,
@@ -65,5 +72,13 @@ if __name__ == '__main__':
         train_dataset=train,
         eval_dataset=dev,
         compute_metrics=compute_metrics,
+        tokenizer=tokenizer,
+        data_collator=data_collator
     )
     trainer.train()
+
+    classifier = pipeline("text-classification", model="stevhliu/my_awesome_model")
+    for instance in test:
+        instance.
+        classifier(text)
+        [{'label': 'POSITIVE', 'score': 0.9994940757751465}]
